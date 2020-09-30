ee8a4885d08e531c028bc7b5f880c64b254b0cc6
TARRAKAN <55315542+TARRAKAN@users.noreply.github.com>
2020-09-30 17:10:25 +0300

    Bees, trees e.t.c.

--- WoodMan/WoodMan.cpp
+++ WoodMan/WoodMan.cpp
@@ -1,13 +1,45 @@
 ﻿#include <SFML/Graphics.hpp>
 
 int main(){
-	sf::VideoMode vidMode(1920, 1080);
-	sf::RenderWindow gameWindow(vidMode,"WoodMan",sf::Style::Fullscreen);
+	sf::VideoMode vidMode(1336, 1080);
+	sf::RenderWindow gameWindow(vidMode,"WoodMan",sf::Style::Resize);
+	sf::Clock clock;
+	//background
 	sf::Texture tBackground;
 	tBackground.loadFromFile("grphcs/background.png");
 	sf::Sprite sBackground;
 	sBackground.setTexture(tBackground);
 	sBackground.setPosition(0, 0);
+	//tree
+	sf::Texture tTree;
+	tTree.loadFromFile("grphcs/tree.png");
+	sf::Sprite sTree;
+	sTree.setTexture(tTree);
+	sTree.setPosition(860, 0);
+	//bee
+	sf::Texture tBee;
+	tBee.loadFromFile("grphcs/bee.png");
+	sf::Sprite sBee;
+	sBee.setTexture(tBee);
+	sBee.setPosition(0, 800);
+	bool beeIsActive = false;
+	float speedOfBee = 0.0;
+	//clouds
+	sf::Texture tCloud;
+	tCloud.loadFromFile("grphcs/cloud.png");
+	sf::Sprite sCloud1, sCloud2, sCloud3;
+	sCloud1.setTexture(tCloud);
+	sCloud2.setTexture(tCloud);
+	sCloud3.setTexture(tCloud);
+	sCloud1.setPosition(0,0);
+	sCloud2.setPosition(0,250);
+	sCloud3.setPosition(0,500);
+	bool Cloud1IsActive = false;
+	bool Cloud2IsActive = false;
+	bool Cloud3IsActive = false;
+	bool speedOfCloud1 = 0.0;
+	bool speedOfCloud2 = 0.0;
+	bool speedOfCloud3 = 0.0;
 
 	while (gameWindow.isOpen()) {
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
@@ -18,10 +50,29 @@ int main(){
 			//
 			//
 			gameWindow.clear();
-			//
+			sf::Time tme = clock.restart();
+			if (!beeIsActive) {
+				srand((int)time(0));
+				speedOfBee = (rand() % 200)+200;
+				srand((int)time(0) * 10);
+				float height = (rand() % 500) + 500;
+				sBee.setPosition(2000, height);
+				beeIsActive = true;
+			} else {
+				sBee.setPosition(sBee.getPosition().x -	(speedOfBee * tme.asSeconds()), sBee.getPosition().y);
+				if (sBee.getPosition().x < -100)
+				{
+					beeIsActive = false;
+				}
+			}
 			//
 			//
 			gameWindow.draw(sBackground);
+			gameWindow.draw(sCloud1);
+			gameWindow.draw(sCloud2);
+			gameWindow.draw(sCloud3);
+			gameWindow.draw(sTree);
+			gameWindow.draw(sBee);
 			//
 			//
 			// 
